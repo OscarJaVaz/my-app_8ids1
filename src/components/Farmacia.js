f72779a014febeb7436a732b4aad0cc2b8b63dbc
Jorge Dolores Vargas <jorge@gmail.com>
2024-02-06 22:14:01 -0600

    implementacion farmacia

--- src/components/Farmacia.js
+++ src/components/Farmacia.js
@@ -1,22 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Button from '@mui/material/Button';
 
 const Farmacia = () => {
-  const [mostrarMensaje, setMostrarMensaje] = useState(true);
+  const estiloEncabezado = {
+    backgroundColor: 'blue',
+    padding: '20px', 
+    color: 'white',
+    textAlign: 'center', 
+    fontSize: '24px', 
+    width: '100%',
+    boxSizing: 'border-box', 
+    margin: 0, 
+    position: 'fixed', 
+    top: 0, 
+    zIndex: 1000, 
+  };
 
-  const cerrarMensaje = () => {
-    setMostrarMensaje(false);
+  const estiloProductosContainer = {
+    backgroundColor: '#CBCBCB', // Agregar fondo de color rojo
+    padding: '30px', // Puedes ajustar el relleno según tus preferencias
+  };
+
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const fetchProductos = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/productos');
+        setProductos(response.data); 
+      } catch (error) {
+        console.error('Error al obtener productos:', error);
+      }
+    };
+
+    fetchProductos();
+  }, []);
+
+  const addToCart = (productId) => {
+    console.log('Producto añadido al carrito con ID:', productId);
   };
 
   return (
-    <div>
-      {mostrarMensaje && (
-        <div>
-          <p>Bienvenido a la farmacia</p>
-        
-        </div>
-      )}
-      {/* Resto de tu contenido de la ventana */}
-    </div>
+    <>
+      <header style={estiloEncabezado}>
+        <nav>
+          BIENVENIDO
+        </nav>
+      </header>
+      <div style={estiloProductosContainer}> {/* Aplicar el estilo al contenedor de productos */}
+        {productos.map(producto => (
+          <div key={producto.id} className="producto">
+            <p></p>
+            <h2 style={{color:'red'}}>{producto.nom_producto}</h2>
+            <img src={producto.imagen} alt={producto.nom_producto} className="producto-imagen" />
+            <p>{producto.descripcion}</p>
+            <p style={{color:'blue'}}>${producto.price}</p>
+            <Button
+              variant="contained"
+              style={{ color: 'white', backgroundColor: 'blue' }}
+              startIcon={<ShoppingCartIcon />}
+              onClick={() => addToCart(producto.id)}>
+              Añadir al carrito
+            </Button>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
