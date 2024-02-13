f324a0290405cc054f3610ba60418ce718856d7e
OscarJaVaz <oscarjandetevazquez@gmail.com>
2024-02-13 12:40:32 -0600

    ajuste menu mostrar citas

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'; // Nuevo icono de usuario
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import paciente from './assets/paciente.png';
 import doctor from './assets/doctor.png';
 import enfermedad from './assets/enfermedad.png';
@@ -12,20 +11,35 @@ import cita from './assets/cita1.png';
 import salir from './assets/salir.png';
 import agregar from './assets/agregar.png';
 import farmacia from './assets/farmacia.png';
-import FullCalendar from '@fullcalendar/react'; // Importa FullCalendar
-import dayGridPlugin from '@fullcalendar/daygrid'; // Importa el plugin de vista de día
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import esLocale from '@fullcalendar/core/locales/es';
+import axios from 'axios';
 
 function MenuComponent() {
   const navigate = useNavigate();
   const [menuVisible, setMenuVisible] = useState(true);
-
-  const handleClick = (path) => {
-    navigate(path);
-  };
+  const [citas, setCitas] = useState([]);
 
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/citas');
+        setCitas(response.data);
+      } catch (error) {
+        console.error('Error fetching citas:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const handleClick = (path) => {
+    navigate(path);
   };
 
   return (
@@ -34,7 +48,7 @@ function MenuComponent() {
         <div className="user-info">
           <h2 style={{ color: 'white' }}>BIENVENIDO</h2>
           <AccountCircleIcon style={{ color: 'white' }} />
-        </div> 
+        </div>
         <p></p>
         <a onClick={() => handleClick("/home")}>
           <img src={paciente} alt="Pacientes" />
@@ -59,7 +73,7 @@ function MenuComponent() {
         <a onClick={() => handleClick("/farmacia")}>
           <img src={farmacia} alt="Farmacia" />
           <span>Farmacia</span>
-        </a>  
+        </a>
         <a onClick={() => handleClick("/")}>
           <img src={salir} alt="Salir" />
           <span>Salir</span>
@@ -69,9 +83,16 @@ function MenuComponent() {
         <IconButton onClick={toggleMenu} className="toggle-button">
           {menuVisible ? <CloseIcon /> : <MenuIcon />}
         </IconButton>
-        {/* Contenido principal va aquí */}
         <div className="calendar-container">
-          <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" locale={esLocale}/>
+          <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            locale={esLocale}
+            events={citas.map(cita => ({
+              title: `${cita.paciente} - ${cita.hora}`,
+              date: cita.fecha 
+            }))}
+          />
         </div>
       </div>
     </div>
