dcfd52419c7f80c39f7d538317ef13d1a6e24d2c
richimx <rgodgom@gmail.com>
2024-02-26 23:31:26 -0600

    Login v2

--- src/components/Login_Component.js
+++ src/components/Login_Component.js
@@ -8,11 +8,11 @@ import InputAdornment from '@mui/material/InputAdornment';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Alert from '@mui/material/Alert';
-import CircularProgress from '@mui/material/CircularProgress';
 import axios from 'axios';
 import secureLocalStorage from 'react-secure-storage';
 import { useNavigate } from 'react-router-dom';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import CircularProgress from '@mui/material/CircularProgress';
 import miImagen from '../components/assets/simbolo_doc.png';
 
 const PasswordField = ({ password, handlePasswordChange }) => {
@@ -51,14 +51,9 @@ const Login_Component = () => {
   const [emptyFieldsError, setEmptyFieldsError] = useState(false);
   const [userNotFoundError, setUserNotFoundError] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [selectedRole, setSelectedRole] = useState('administrador');
 
   const handleInputChange = (event, setState) => {
     setState(event.target.value);
-  };
-
-  const handleRoleChange = (event) => {
-    setSelectedRole(event.target.value);
   };
 
   const handleClose = (event, reason) => {
@@ -82,11 +77,11 @@ const Login_Component = () => {
 
     try {
       const response = await axios.post(
-        selectedRole === 'administrador' ? 'http://127.0.0.1:8000/api/login' : 'http://127.0.0.1:8000/api/logincliente',
-        selectedRole === 'administrador' ? { email, password } : { email, contrasena: password }
+        'http://127.0.0.1:8000/api/login',
+        { email, password }
       );
 
-      console.log('Validando Acceso..');
+      console.log('Validating Access..');
       console.log(response.data);
 
       if (response.data.token !== '') {
@@ -94,10 +89,27 @@ const Login_Component = () => {
         secureLocalStorage.setItem('token', response.data.token);
         secureLocalStorage.setItem('username', response.data.nombre);
         setOpen(true);
-        navigate(selectedRole === 'administrador' ? '/menu' : '/cliente');
+        navigate('/menu');
       } else {
-        setUserNotFoundError(true);
-        setOpen(false);
+        // Si la primera API no devuelve un token válido, intentar con la segunda API
+        const secondResponse = await axios.post(
+          'http://127.0.0.1:8000/api/logincliente',
+          { email, contrasena: password }
+        );
+
+        console.log('Second API Response:');
+        console.log(secondResponse.data);
+
+        if (secondResponse.data.token !== '') {
+          console.log('OK');
+          secureLocalStorage.setItem('token', secondResponse.data.token);
+          secureLocalStorage.setItem('username', secondResponse.data.nombre);
+          setOpen(true);
+          navigate('/cliente');
+        } else {
+          setUserNotFoundError(true);
+          setOpen(false);
+        }
       }
     } catch (error) {
       console.error(error);
@@ -142,9 +154,10 @@ const Login_Component = () => {
                   },
                 }}
                 onClick={fnLogin}
-                disabled={!email || !password}
+                disabled={!email || !password || loading} // Deshabilita el botón mientras se carga
               >
-                <span>Entrar</span>
+                {loading && <CircularProgress size={24} color="inherit" />} {/* Muestra la rueda de carga cuando loading es verdadero */}
+                {!loading && <span>Entrar</span>} {/* Muestra "Entrar" cuando loading es falso */}
                 <LockOutlinedIcon style={{ marginLeft: '5px' }} />
               </Button>
             </Stack>
@@ -181,51 +194,6 @@ const Login_Component = () => {
                 Usuario o contraseña incorrectos
               </Alert>
             )}
-            <div style={{ display: 'flex', flexDirection: 'column', marginTop: '10px', alignItems: 'center', color: '#000000', fontWeight: 'bold' }}>
-              <label style={{ fontSize: '26px', marginBottom: '5px' }}>Identificate:</label>
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <input
-                  type="radio"
-                  id="admin"
-                  name="role"
-                  value="administrador"
-                  checked={selectedRole === 'administrador'}
-                  onChange={handleRoleChange}
-                  style={{ marginRight: '5px' }}
-                />
-                <label htmlFor="admin" style={{ fontSize: '14px' }}>Administrador</label>
-              </div>
-              <div style={{ display: 'flex', alignItems: 'center', marginTop: '5px' }}>
-                <input
-                  type="radio"
-                  id="client"
-                  name="role"
-                  value="cliente"
-                  checked={selectedRole === 'cliente'}
-                  onChange={handleRoleChange}
-                  style={{ marginRight: '5px' }}
-                />
-                <label htmlFor="client" style={{ fontSize: '14px' }}>Cliente</label>
-              </div>
-              <div style={{ alignSelf: 'flex-end', marginRight: '100px', marginTop: '50px', marginBottom: '20px' }}>
-                <Button
-                  variant="contained"
-                  sx={{
-                    fontFamily: 'sans-serif',
-                    fontWeight: 'bold',
-                    backgroundColor: '#FFC107',
-                    color: '#ffffff',
-                    width: '200px',
-                    '&:hover': {
-                      backgroundColor: '#FF5252',
-                    },
-                  }}
-                  onClick={() => handleNavigation('/')}
-                >
-                  <span>Regresar</span>
-                </Button>
-              </div>
-            </div>
           </Stack>
         </div>
         <div style={{ marginLeft: '20px', alignSelf: 'flex-end', marginBottom: '20px' }}>
--- src/components/VistaCompra.js
+++ src/components/VistaCompra.js
@@ -16,82 +16,72 @@ const VistaCompra = () => {
     expiry: '',
     cvc: '',
     focus: '',
-    address: '',
+  });
+  const [addressDetails, setAddressDetails] = useState({
     postalCode: '',
+    address: '',
     city: '',
-    country: '',
+    referencia: '',
   });
 
   const [carrito, setCarrito] = useState(state ? [...state.carrito] : []);
+  const [nombreCliente, setNombreCliente] = useState('');
 
-  const handleInputChange = (e) => {
-    setPaymentDetails({
-      ...paymentDetails,
+  const handleInputChange = (e, setter) => {
+    setter({
+      ...setter,
       [e.target.name]: e.target.value,
     });
   };
 
-  const handleFocusChange = (e) => {
-    setPaymentDetails({
-      ...paymentDetails,
+  const handleFocusChange = (e, setter) => {
+    setter({
+      ...setter,
       focus: e.target.name,
     });
   };
 
-  const processPayment = () => {
-    console.log('Payment Details:', JSON.stringify(paymentDetails));
-    // Aquí puedes agregar la lógica para procesar el pago
-    // Por ahora, solo mostraremos la modal
-    setShowModal(true);
-  };
+  const processPayment = (e) => {
+    e.preventDefault(); // Evitar que se recargue la página al enviar el formulario
 
-  const handlePostalCodeChange = (e) => {
-    const postalCode = e.target.value;
-    // Aquí implementamos la lógica para rellenar la información de la dirección
-    // Ejemplo de cómo podrías hacerlo
-    if (postalCode === '55773' || postalCode === '55755') {
-      setPaymentDetails({
-        ...paymentDetails,
-        postalCode,
-        address: 'Calle Cerrada 20 de Noviembre',
-        municipio: 'Tecámac',
+    const datosCompra = {
+      ...paymentDetails,
+      ...addressDetails,
+      nombre_cliente: nombreCliente,
+      total: calcularTotal(),
+      cantidad: calcularCantidadProductos(),
+    };
+
+    // Enviar los datos al backend
+    fetch('http://127.0.0.1:8000/api/compra/crear', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(datosCompra),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Respuesta del servidor:', data);
+        setShowModal(true); // Mostrar la modal de confirmación
+      })
+      .catch(error => {
+        console.error('Error al procesar el pago:', error);
+        // Manejar el error
       });
-    } else {
-      setPaymentDetails({
-        ...paymentDetails,
-        postalCode,
-        address: '',
-        municipio: '',
-      });
-    }
   };
 
-  // Estado para controlar la visibilidad de la modal
-  const [showModal, setShowModal] = useState(false);
-  const [showPaymentForm, setShowPaymentForm] = useState(false);
-  const [showAddressForm, setShowAddressForm] = useState(false);
-
-  const handleContinueToPayment = () => {
-    setShowAddressForm(true);
+  const calcularTotal = () => {
+    return carrito.reduce((total, producto) => total + producto.quantity * producto.price, 0);
   };
 
-  const handleAddressFormSubmit = () => {
-    // Aquí puedes agregar la lógica para validar y procesar los datos de dirección
-    setShowAddressForm(false);
+  const calcularCantidadProductos = () => {
+    return carrito.reduce((total, producto) => total + producto.quantity, 0);
+  };
+
+  const handleAddressFormSubmit = (e) => {
+    e.preventDefault();
     setShowPaymentForm(true);
-  };
-
-  const handleQuantityChange = (productId, newQuantity) => {
-    if (newQuantity <= 0) {
-      // Eliminar el producto del carrito si la cantidad es menor o igual a cero
-      const updatedCart = carrito.filter(item => item.id !== productId);
-      setCarrito(updatedCart);
-    } else {
-      const updatedCart = carrito.map(item =>
-        item.id === productId ? { ...item, quantity: newQuantity } : item
-      );
-      setCarrito(updatedCart);
-    }
   };
 
   const removeFromCart = (productId) => {
@@ -99,7 +89,10 @@ const VistaCompra = () => {
     setCarrito(updatedCart);
   };
 
-  // Configuración del carrusel
+  const [showModal, setShowModal] = useState(false);
+  const [showPaymentForm, setShowPaymentForm] = useState(false);
+  const [showAddressForm, setShowAddressForm] = useState(false);
+
   const sliderSettings = {
     dots: true,
     infinite: false,
@@ -107,9 +100,6 @@ const VistaCompra = () => {
     slidesToShow: 3,
     slidesToScroll: 1
   };
-
-  // Calcular el total del precio de los productos en el carrito
-  const totalPrecio = carrito.reduce((total, producto) => total + (producto.quantity * producto.price), 0);
 
   return (
     <div className="layout">
@@ -122,151 +112,72 @@ const VistaCompra = () => {
               <img src={producto.imagen} alt={producto.nom_producto} className="carrito-imagen" />
               <p>Descripción: {producto.descripcion}</p>
               <p>Cantidad:
-                <button onClick={() => handleQuantityChange(producto.id, producto.quantity - 1)} className="btn-cantidad">-</button>
-                {producto.quantity}
-                <button onClick={() => handleQuantityChange(producto.id, producto.quantity + 1)} className="btn-cantidad">+</button>
+                <button onClick={() => removeFromCart(producto.id)} className="btn-eliminar">Eliminar</button>
               </p>
               <p>Precio Total: ${producto.quantity * producto.price}</p>
-              <button onClick={() => removeFromCart(producto.id)} className="btn-eliminar">Eliminar</button>
             </div>
           ))}
         </Slider>
         {/* Mostrar el total del precio */}
-        <p className="total-price">Precio total: ${totalPrecio}</p>
+        <p className="total-price">Precio total: ${calcularTotal()}</p>
       </div>
       <div className="col-md-6">
-        {!showPaymentForm && !showAddressForm && (
-          <button onClick={handleContinueToPayment} type="button" className="btn btn-primary btn-lg btn-block">Continuar con el Pago</button>
-        )}
-        {showAddressForm && (
+        <form onSubmit={processPayment}>
           <div className="alert alert-info" role="alert">
             <h4 className="alert-heading" style={{ color: '#305edb', marginBottom: '20px', fontWeight: 'bold', fontSize: '24px' }}>Ingresa tus datos domiciliarios</h4>
-
-            <form onSubmit={handleAddressFormSubmit}>
-             
-              <div className="form-row">
-                <div className="form-group col-md-6">
-                  <label htmlFor="postalCode">Código Postal</label>
-                  <input
-                    type="text"
-                    name="postalCode"
-                    id="postalCode"
-                    className="form-control"
-                    value={paymentDetails.postalCode}
-                    onChange={handleInputChange}
-                    onBlur={handlePostalCodeChange}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                <div className="form-group col-md-6">
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label htmlFor="postalCode">Código Postal</label>
+                <input
+                  type="text"
+                  name="postalCode"
+                  id="postalCode"
+                  className="form-control"
+                  value={addressDetails.postalCode}
+                  onChange={(e) => handleInputChange(e, setAddressDetails)}
+                  required
+                />
+              </div>
+              <div className="form-group col-md-6">
                 <label htmlFor="address">Dirección</label>
                 <input
                   type="text"
                   name="address"
                   id="address"
                   className="form-control"
-                  value={paymentDetails.address}
-                  onChange={handleInputChange}
+                  value={addressDetails.address}
+                  onChange={(e) => handleInputChange(e, setAddressDetails)}
                   required
                 />
               </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="city">Municipio</label>
+                <input
+                  type="text"
+                  name="city"
+                  id="city"
+                  className="form-control"
+                  value={addressDetails.city}
+                  onChange={(e) => handleInputChange(e, setAddressDetails)}
+                  required
+                />
               </div>
-                <div className="form-group col-md-6">
-                  <label htmlFor="city">Municipio</label>
-                  <input
-                    type="text"
-                    name="city"
-                    id="city"
-                    className="form-control"
-                    value={paymentDetails.municipio}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </div>
-                <div className="form-group col-md-6">
-                  <label htmlFor="city">Referencia</label>
-                  <input
-                    type="text"
-                    name="referencia"
-                    id="referencia"
-                    className="form-control"
-                    onChange={handleInputChange}
-                    required
-                  />
-                </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="referencia">Referencia</label>
+                <input
+                  type="text"
+                  name="referencia"
+                  id="referencia"
+                  className="form-control"
+                  value={addressDetails.referencia}
+                  onChange={(e) => handleInputChange(e, setAddressDetails)}
+                  required
+                />
               </div>
-            
-              <button type="submit" className="btn btn-success btn-block btn-lg">Continuar con el Pago</button>
-            </form>
-          </div>
-        )}
-        {showPaymentForm && (
-          <div className="card">
-            <div className="card-body">
-              <Cards
-                number={paymentDetails.number}
-                name={paymentDetails.name}
-                expiry={paymentDetails.expiry}
-                cvc={paymentDetails.cvc}
-                focused={paymentDetails.focus}
-              />
-              <form>
-                <div className="form-group">
-                  <label htmlFor="number">Número de la tarjeta</label>
-                  <input
-                    type="text"
-                    name="number"
-                    id="number"
-                    maxLength="16"
-                    className="form-control"
-                    onChange={handleInputChange}
-                    onFocus={handleFocusChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="name">Nombre del Titular</label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    maxLength="30"
-                    className="form-control"
-                    onChange={handleInputChange}
-                    onFocus={handleFocusChange}
-                  />
-                </div>
-                <div className="form-row">
-                  <div className="form-group col-md-6">
-                    <label htmlFor="expiry">Fecha de expiración</label>
-                    <input
-                      type="text"
-                      name="expiry"
-                      id="expiry"
-                      maxLength="4"
-                      className="form-control"
-                      onChange={handleInputChange}
-                      onFocus={handleFocusChange}
-                    />
-                  </div>
-                  <div className="form-group col-md-6">
-                    <label htmlFor="cvc">CVC</label>
-                    <input
-                      type="text"
-                      name="cvc"
-                      id="cvc"
-                      maxLength="4"
-                      className="form-control"
-                      onChange={handleInputChange}
-                      onFocus={handleFocusChange}
-                    />
-                  </div>
-                </div>
-                <button onClick={processPayment} type="button" className="btn btn-success btn-block btn-lg">Pagar</button>
-              </form>
             </div>
+            <button type="submit" className="btn btn-success btn-block btn-lg">Continuar con el Pago</button>
           </div>
-        )}
+        </form>
       </div>
       {showModal && (
         <div>
