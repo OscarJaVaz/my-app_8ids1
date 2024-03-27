25e6ab145e2f452be83d1b691a44e5e45267e834
BallitoBb <2520160010aavilesr@gmail.com>
2024-03-27 17:14:42 -0600

    Diseño-Validacion_Perfiles

--- src/components/Perfildoc.js
+++ src/components/Perfildoc.js
@@ -4,20 +4,22 @@ import secureLocalStorage from 'react-secure-storage';
 import { Link } from 'react-router-dom';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
-import { Container, Typography, TextField, Button, Grid, Box, Divider, Paper, Slide, Avatar, IconButton } from '@mui/material';
+import { Typography, TextField, Button, Grid, Box, Divider, Paper, Slide, Avatar } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 import { Person, PersonOutline } from '@mui/icons-material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+//CSS
+import './Perfil_Estilo.css';
 
 const Perfildoc = () => {
     const [userData, setUserData] = useState(null);
-    const [user, setUser] = useState(null);
     const [currentPassword, setCurrentPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [isChangingPassword, setIsChangingPassword] = useState(false);
     const [successMessage, setSuccessMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const [openSnackbar, setOpenSnackbar] = useState(false);
-    const [isFemale, setIsFemale] = useState(false); // Variable para determinar si el paciente es femenino o no
+    const [isFemale, setIsFemale] = useState(false);
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -28,18 +30,14 @@ const Perfildoc = () => {
                 }
 
                 const response = await axios.get('http://127.0.0.1:8000/api/perfildoc', {
-                    params: {
-                        nombre_usuario: storedUsername
-                    }
+                    params: { nombre_usuario: storedUsername }
                 });
 
-                setUser(storedUsername);
                 setUserData(response.data);
 
-                // Verificar si el nombre del paciente es femenino
                 if (response.data && response.data.nombre) {
                     const nombre = response.data.nombre.toLowerCase();
-                    setIsFemale(nombre.endsWith('a')); // Si el nombre termina con 'a', se considera femenino
+                    setIsFemale(nombre.endsWith('a'));
                 }
             } catch (error) {
                 console.error('Error al obtener los datos del usuario:', error);
@@ -57,7 +55,7 @@ const Perfildoc = () => {
                 password_actual: currentPassword,
                 nueva_password: newPassword
             });
-            
+
             setSuccessMessage(response.data.message);
             setErrorMessage('');
             setOpenSnackbar(true);
@@ -81,98 +79,116 @@ const Perfildoc = () => {
     };
 
     return (
-        
-        <Container maxWidth="md">
-            <Box mt={5} mb={3} textAlign="center">
-                <Typography variant="h4" gutterBottom>Datos del Doctor</Typography>
+        <div className="container">
+            <header className="perfil">
                 {userData && (
                     <Avatar sx={{ width: 100, height: 100, backgroundColor: isFemale ? 'pink' : 'blue', margin: 'auto' }}>
-                        {isFemale ? <PersonOutline sx={{ width: 80, height: 80 }} /> : <Person sx={{ width: 80, height: 80 }} />}
+                        {isFemale ? <PersonOutline sx={{ width: 100, height: 100 }} /> : <Person sx={{ width: 100, height: 100 }} />}
                     </Avatar>
                 )}
-            </Box>
-            {user && (
-                <Grid container spacing={3} justifyContent="center">
-                    <Grid item xs={12} md={6}>
-    <Slide direction="right" in={true} timeout={1000}>
-        <Paper elevation={3} sx={{ bgcolor: 'primary.main', color: 'white', p: 3, textAlign: 'center', borderRadius: 4, boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }}>
-            {userData ? (
-                <div>
-                    <Divider sx={{ mb: 2 }} />
-                    <Typography variant="subtitle1" gutterBottom>Nombre del doctor: {userData.name}</Typography>
-                    <Typography variant="subtitle1" gutterBottom>Correo electronico: {userData.email}</Typography>
+                <div className="titulo">
+                    <h1>Datos del Paciente</h1>
                 </div>
-            ) : (
-                <Typography variant="body1" gutterBottom>No se encontraron datos del usuario.</Typography>
-            )}
-        </Paper>
-    </Slide>
-</Grid>
+            </header>
 
-                    <Grid item xs={12} md={6}>
-                        <Slide direction="left" in={true} timeout={1000}>
-                            <Paper elevation={3} sx={{ bgcolor: 'info.main', p: 3 }}>
-                                {isChangingPassword ? (
-                                    <div>
-                                        <Typography variant="subtitle1" gutterBottom align="center">Cambiar Contraseña</Typography>
-                                        <TextField
-                                            variant="outlined"
-                                            fullWidth
-                                            type="password"
-                                            label="Contraseña actual"
-                                            value={currentPassword}
-                                            onChange={(e) => setCurrentPassword(e.target.value)}
-                                            sx={{ mb: 2 }}
-                                        />
-                                        <TextField
-                                            variant="outlined"
-                                            fullWidth
-                                            type="password"
-                                            label="Nueva Contraseña"
-                                            value={newPassword}
-                                            onChange={(e) => setNewPassword(e.target.value)}
-                                            sx={{ mb: 2 }}
-                                        />
-                                        <Button variant="contained" onClick={handleActualizarContraseña} fullWidth>Actualizar Contraseña</Button>
-                                    </div>
-                                ) : (
-                                    <div>
-                                        
-                                        <Typography variant="body1" gutterBottom align="center">¿Deseas cambiar tu contraseña?</Typography>
-                                        <Button variant="contained" onClick={() => setIsChangingPassword(true)} fullWidth>Cambiar Contraseña</Button>
-                                    </div>
-                                )}
-                            </Paper>
-                        </Slide>
+            <main className="projetos">
+                <Box mt={2} mb={2}>
+                    <Grid container spacing={2} justifyContent="center">
+                        <Grid item xs={12} md={10}>
+                            <Slide direction="right" in={true} timeout={1000}>
+                                <Paper elevation={5} sx={{ bgcolor: 'white', p: 3, color: 'black' }}>
+                                    {userData ? (
+                                        <div>
+                                            <Typography variant="h6" gutterBottom align="center">Información</Typography>
+                                            <Divider sx={{ mb: 2 }} />
+                                            <Typography variant="subtitle1" gutterBottom>Nombre del paciente:</Typography>
+                                            <Typography variant="subtitle2" gutterBottom>{userData.name}</Typography>
+                                            <Typography variant="subtitle1" gutterBottom>Email:</Typography>
+                                            <Typography variant="subtitle2" gutterBottom>{userData.email}</Typography>
+                                        </div>
+                                    ) : (
+                                        <Typography variant="body1" gutterBottom>No se encontraron datos del usuario.</Typography>
+                                    )}
+                                </Paper>
+                            </Slide>
+                        </Grid>
+
+                        <Grid item xs={12} md={10}>
+                            <Slide direction="left" in={true} timeout={1000}>
+                                <Paper elevation={5} sx={{ bgcolor: 'white', p: 3, color: 'black' }}>
+                                    {isChangingPassword ? (
+                                        <div>
+                                            <Typography variant="h6" gutterBottom align="center">Cambiar Contraseña</Typography>
+                                            <TextField
+                                                variant="outlined"
+                                                fullWidth
+                                                type="password"
+                                                label="Contraseña actual"
+                                                value={currentPassword}
+                                                onChange={(e) => setCurrentPassword(e.target.value)}
+                                                sx={{ mb: 2 }}
+                                            />
+                                            <TextField
+                                                variant="outlined"
+                                                fullWidth
+                                                type="password"
+                                                label="Nueva Contraseña"
+                                                value={newPassword}
+                                                onChange={(e) => setNewPassword(e.target.value)}
+                                                sx={{ mb: 2 }}
+                                            />
+                                            <Button variant="contained" onClick={handleActualizarContraseña} fullWidth>Actualizar Contraseña</Button>
+                                        </div>
+                                    ) : (
+                                        <div>
+                                            <Typography variant="body1" gutterBottom align="center">¿Deseas cambiar tu contraseña?</Typography>
+                                            <Button variant="contained" onClick={() => setIsChangingPassword(true)} fullWidth>Cambiar Contraseña</Button>
+                                        </div>
+                                    )}
+                                </Paper>
+                            </Slide>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                        <Snackbar
-                            open={openSnackbar}
-                            autoHideDuration={6000}
+                </Box>
+
+                <Grid item xs={12}>
+                    <Snackbar
+                        open={openSnackbar}
+                        autoHideDuration={6000}
+                        onClose={handleCloseSnackbar}
+                        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                    >
+                        <MuiAlert
                             onClose={handleCloseSnackbar}
-                            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                            severity={successMessage ? 'success' : 'error'}
+                            sx={{ width: '100%', backgroundColor: 'white', color: 'black' }}
                         >
-                            <MuiAlert
-                                onClose={handleCloseSnackbar}
-                                severity={successMessage ? 'success' : 'error'}
-                                sx={{
-                                    width: '100%',
-                                    backgroundColor: 'white',
-                                    color: 'black'
-                                }}
-                            >
-                                {successMessage || errorMessage}
-                            </MuiAlert>
-                        </Snackbar>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Link to="/menu" style={{ textDecoration: 'none' }}>
-                            <Button variant="outlined" fullWidth>Regresar</Button>
-                        </Link>
-                    </Grid>
+                            {successMessage || errorMessage}
+                        </MuiAlert>
+                    </Snackbar>
                 </Grid>
-            )}
-        </Container>
+            </main>
+
+            <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Link to="/menu" style={{ textDecoration: 'none' }}>
+                    <Button
+                        variant="outlined"
+                        sx={{
+                            width: '200px',
+                            color: 'black',
+                            backgroundColor: 'orange',
+                            '&:hover': {
+                                backgroundColor: 'rgba(255, 165, 0, 0.8)',
+                            }
+                        }}
+                        startIcon={<ArrowBackIcon />}
+                    >
+                        Regresar
+                    </Button>
+                </Link>
+            </Grid>
+
+        </div>
     );
 };
 
